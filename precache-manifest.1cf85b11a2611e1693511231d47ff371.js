a29cda6a6cfe6623b5377e1cd096befdb50a4fe1
SaiAshish9 <saiashish3760@gmail.com>
2019-07-24 16:02:48 +0530

    Updates

---
+++ precache-manifest.1cf85b11a2611e1693511231d47ff371.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bc04ae364c84ced47e41289dc825ea",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "513447d0e2b871b9d427",
+    "url": "/RecipeApp/static/css/main.476a27f7.chunk.css"
+  },
+  {
+    "revision": "0318eb5aadc6bb66851b",
+    "url": "/RecipeApp/static/js/2.ce2d1ea8.chunk.js"
+  },
+  {
+    "revision": "513447d0e2b871b9d427",
+    "url": "/RecipeApp/static/js/main.8a3354e5.chunk.js"
+  },
+  {
+    "revision": "fd6505cb64c26bbb899e",
+    "url": "/RecipeApp/static/js/runtime~main.4339c778.js"
+  }
+]);
